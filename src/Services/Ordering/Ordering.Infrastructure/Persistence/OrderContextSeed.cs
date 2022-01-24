5b3e07c810da9f31c852e59a4b3678d5c10aa3bf
gokhanphlvn <gokhanpehlivan@outlook.com>
2022-01-24 23:55:34 +0300

    Ordering.API

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {
+                    UserName = "swn",
+                    FirstName = "Gökhan",
+                    LastName = "Pehlivan",
+                    EmailAddress = "gokhanpehlivan06@gmail.com",
+                    AddressLine = "Bahcelievler",
+                    Country = "Turkey",
+                    TotalPrice = 350,
+                    State ="a",
+                    ZipCode="a",
+                    CardName="a",
+                    CardNumber="a",
+                    Expiration="a",
+                    CVV="a",
+                    LastModifiedBy="swn",
+                    LastModifiedDate=DateTime.Now
+                }
+            };
+        }
+    }
+}
