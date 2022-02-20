eb17c2a9ce545b73c3a3ebd7bcef186b82d39291
gokhanphlvn <gokhanpehlivan@outlook.com>
2022-02-21 00:04:01 +0300

    Api Gateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,20 +1,24 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+}).ConfigureLogging((hostingContext, loggingBuilder) =>
 {
     loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("logging"));
     loggingBuilder.AddConsole();
     loggingBuilder.AddDebug();
 });
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello ApiGateways With Ocelot!");
 
 await app.UseOcelot();
 
