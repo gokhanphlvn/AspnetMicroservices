7b01b38e61f39eddd51242e2ae3f83125e7258a1
gokhanphlvn <gokhanpehlivan@outlook.com>
2022-02-26 18:36:13 +0300

    Shopping.Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -0,0 +1,46 @@
+using Common.Logging;
+using Shopping.Aggregator.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<LoggingDelegatingHandler>();
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]))
+    .AddHttpMessageHandler<LoggingDelegatingHandler>();
+//.AddPolicyHandler(GetRetryPolicy())
+//.AddPolicyHandler(GetCircuitBreakerPolicy());
+
+builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]))
+    .AddHttpMessageHandler<LoggingDelegatingHandler>();
+//.AddPolicyHandler(GetRetryPolicy())
+//.AddPolicyHandler(GetCircuitBreakerPolicy());
+
+builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]))
+    .AddHttpMessageHandler<LoggingDelegatingHandler>();
+    //.AddPolicyHandler(GetRetryPolicy())
+    //.AddPolicyHandler(GetCircuitBreakerPolicy());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
